8a749da596357ee4a44b82e36e6f63e0e4b949dc
Rush-IT <78534165+Rush-IT@users.noreply.github.com>
2025-07-25 02:27:18 +0500

    Add class DownloadImage in ViewModels

---
+++ TestingTask/TestingTask/ViewModels/DownloadImage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TestingTask.ViewModels
+{
+    class DownloadImage
+    {
+        public ImageSource Image { get; set; }
+        public string Url { get; set; }
+        public double Progress { get; set; }
+        public string Status { get; set; }
+
+        public ICommand DownloadCommand { get; }
+        public ICommand PauseCommand { get; }
+
+        private CancellationTokenSource canselTokenSource;
+
+
+        public DownloadImage()
+        {
+            
+        }
+        public async Task Download()
+        {
+            Status = "Загрузка...";
+            try
+            {
+                ImageSource result =  // Начало загрузки...
+                Image = result;
+                Status = "Готово";
+            }
+            catch (OperationCanceledException)
+            {
+                Status = "Отменено";
+            }
+            catch(Exception ex)
+            {
+                Status = "Ошибка: " + ex.Message;
+            }
+        }
+    }
+}
